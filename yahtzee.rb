04d8ca237a49a2a4218083d4baf742d0b4d6fd6a
Richi Ruiz <ruizrichard09@gmail.com>
2017-11-03 12:07:36 -0600

    there you go jj

--- yahtzee.rb
+++ yahtzee.rb
@@ -1,3 +1,4 @@
+require "pry"
 class ScoreCard
 end
 
@@ -6,11 +7,35 @@ end
 
 
 class Game
-  
-  def roll
-    
-  end
 
+    def roll
+    
+        @die = (1..6).to_a
+        @dicecup = []
+        @die.sample
+        until @dicecup.count == 5 
+        @dicecup << @die.sample
+    end
+
+    @dicecup
+
+    def select
+        @dicecupe
+        binding.pry
+        # what ever one(s) they pick roll the rest
+        @dicecup.select {@dicecup} || @dicecup.count == 5
+        binding.pry
+
+    end
+    @dicecup
+
+
+
+    
+end
+
+  
+  
   def player_turn
     
   end
@@ -19,3 +44,6 @@ class Game
     
   end
 end  
+my_game = Game.new
+p my_game.roll
+p "Type the dies you want to keep"
